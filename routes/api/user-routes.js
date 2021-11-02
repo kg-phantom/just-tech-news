e85469e09fb001990b294bf6a733e3fff947eb4a
Kyrsten <ksurbayan@gmail.com>
2021-11-02 14:59:04 -0800

    hashed and verify password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -69,6 +69,30 @@ router.put('/:id', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        //res.json({ user: dbUserData });
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
     User.destroy({
